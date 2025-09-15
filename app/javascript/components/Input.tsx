16bc5f5a72707671481c003b3246df84d86cf7df
Ayush Sharma <ayush.sharma74892@gmail.com>
2025-09-16 01:41:17 +0530

    use semantic colour classes insted of hex codes

--- app/javascript/components/Input.tsx
+++ app/javascript/components/Input.tsx
@@ -8,8 +8,8 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       className={classNames(
         "file:text-foreground flex w-full min-w-0 rounded-sm border border-black bg-transparent px-4 py-9 text-base outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[16px] placeholder:text-zinc-500 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:border-zinc-600 dark:bg-black",
-        "focus-visible:border-black focus-visible:ring-[#FF90E8]",
-        "aria-invalid:ring-[#DC341E] dark:aria-invalid:ring-[#DC341E] aria-invalid:border-[#DC341E] dark:aria-invalid:border-[#DC341E]",
+        "focus-visible:border-black focus-visible:ring-pink",
+        "aria-invalid:ring-red dark:aria-invalid:ring-red aria-invalid:border-red dark:aria-invalid:border-red",
         className,
       )}
       {...props}
